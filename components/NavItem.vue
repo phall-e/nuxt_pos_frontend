deef9ef6d14aa20c2ceaedd2cb6d5281d9d12b41
phall-e <eomphall@gmail.com>
2026-01-14 18:47:00 +0700

    rf: improve on navigation bar items and user permissions

--- components/NavItem.vue
+++ components/NavItem.vue
@@ -1,33 +1,34 @@
 <template>
-   <NuxtLink
-    v-if="item.moduleName"
-    :to="item.to"
-    class="text-white flex items-center"
-    active-class="!text-red-500"
-    v-can="`read-${item.moduleName}`"
-  >
-    <div class="flex items-center justify-center" :class="collapse.isCollapsed ? 'h-[50px] w-[50px]' : 'h-[40px] w-[40px]'">
-      <Icon :size="20" :name="item.icon"/>
-    </div>
-    <div v-if="!collapse.isCollapsed" class="text-[15px]">{{ $t(item.title) }}</div>
-  </NuxtLink>
   <NuxtLink
-    v-else
+    v-if="canRead"
     :to="item.to"
     class="text-white flex items-center"
     active-class="!text-red-500"
   >
-    <div class="flex items-center justify-center" :class="collapse.isCollapsed ? 'h-[50px] w-[50px]' : 'h-[40px] w-[40px]'">
-      <Icon :size="20" :name="item.icon"/>
+    <div
+      class="flex items-center justify-center"
+      :class="collapse.isCollapsed ? 'h-[50px] w-[50px]' : 'h-[40px] w-[40px]'"
+    >
+      <Icon :size="20" :name="item.icon" />
     </div>
-    <div v-if="!collapse.isCollapsed" class="text-[15px]">{{ $t(item.title) }}</div>
+    <div v-if="!collapse.isCollapsed" class="text-[15px]">
+      {{ $t(item.title) }}
+    </div>
   </NuxtLink>
 </template>
 
 <script setup lang="ts">
-import type { NavigationItem } from '~/types/NavigationItem';
+import type { NavigationItem } from '~/types/NavigationItem'
 
-defineProps<{ item: NavigationItem }>()
+const props = defineProps<{ item: NavigationItem }>()
 
-const collapse = useCollapseStore();
+const collapse = useCollapseStore()
+const permissionStore = usePermissionStore()
+
+const canRead = computed(() => {
+  if (!props.item.moduleName) return true
+  return permissionStore.permissions?.includes(
+    `read-${props.item.moduleName}`
+  )
+})
 </script>
--- plugins/can.client.ts
+++ plugins/can.client.ts
@@ -1,13 +1,32 @@
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.directive('can', {
-        mounted(el, binding) {
-            const auth = useAuthenticationStore();
-            const permission = usePermissionStore();
-            if (!auth.users.isAdmin) {
-                if (!permission.can(binding.value)) {
-                    el.style.display = 'none';
-                }
-            }
+  nuxtApp.vueApp.directive('can', {
+    mounted(el, binding) {
+      const auth = useAuthenticationStore()
+      const permission = usePermissionStore()
+
+      if (!auth.users?.isAdmin) {
+        if (!permission.can(binding.value)) {
+          el.style.display = 'none'
         }
-    })
+      }
+    },
+
+    /**
+     * ✅ REQUIRED for SSR
+     */
+    getSSRProps(binding) {
+      const auth = useAuthenticationStore()
+      const permission = usePermissionStore()
+
+      if (!auth.users?.isAdmin) {
+        if (!permission.can(binding.value)) {
+          return {
+            style: { display: 'none' },
+          }
+        }
+      }
+
+      return {}
+    },
+  })
 })
